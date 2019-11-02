f8bee4ff0c807da74d2012174793dfde3bd68e56
vitor-k <vitor-kiguchi@hotmail.com>
2019-11-02 16:27:48 -0300

    Initial multiple models approach (wouldn't be able to have a single log in screen for all users)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :representante_externos
+  devise_for :admins
+  devise_for :docentes
+  devise_for :alunos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
---
+++ test/models/representante_externo_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RepresentanteExternoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
