7a3d42a4fc9d68b0b65b596a9d75ea4d1b15a6bb
Pedro <pbcarvalhaes@gmail.com>
2019-11-11 15:25:35 -0300

    Atualização da criação de postagens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_144532) do
+ActiveRecord::Schema.define(version: 2019_11_11_181455) do
 
   create_table "admins", force: :cascade do |t|
     t.integer "nusp"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2019_11_10_144532) do
     t.integer "laboratorio_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "titulo"
     t.index ["admin_id"], name: "index_postagems_on_admin_id"
     t.index ["aluno_id"], name: "index_postagems_on_aluno_id"
     t.index ["docente_id"], name: "index_postagems_on_docente_id"
