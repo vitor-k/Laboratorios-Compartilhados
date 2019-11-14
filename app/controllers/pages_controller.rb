6cc818a7d5310bbd92c2beddb3e05b851a2c2f3c
Vinicius <massaki1999@gmail.com>
2019-11-14 10:10:59 -0300

    vinculos dos docentes funcionando

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,15 +7,10 @@ class PagesController < ApplicationController
   end
 
   def account
-    if (current_user.docente?)
-      docente = (Docente.find(current_user.meta_id))
-      @laboratorio_docente = Laboratorio.all
-      @laboratorio_docente.find_each do |cada|
-        if (cada.docentes.where.not(id: docente.id).exists?)
-          @laboratorio_docente.drop(cada.id)
-        end
+    if (docente_signed_in?)
+      @docente = Docente.find(current_user.meta_id)
+      @laboratorio_docente = @docente.laboratorios
       end
     end
-  end
 
 end
