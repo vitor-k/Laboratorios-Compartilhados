4daead61cdf1c3e8c4c53db5e9dd491f303b45f2
Vinicius <massaki1999@gmail.com>
2019-11-19 21:15:35 -0300

    uniqueness

--- app/models/representante_externo.rb
+++ app/models/representante_externo.rb
@@ -3,7 +3,7 @@ class RepresentanteExterno < ApplicationRecord
   has_one :user, as: :meta, dependent: :destroy
   accepts_nested_attributes_for :user
   
-  validates :RG, presence: true
+  validates :RG, presence: true, uniqueness: true
   validates :RG, numericality: { only_integer: true }
   validates :UF, presence: true
 end
