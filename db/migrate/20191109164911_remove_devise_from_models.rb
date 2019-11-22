a060227c8540dba8583ea9b5efaa0343bdcf78f3
vitor-k <vitor-kiguchi@hotmail.com>
2019-11-22 12:39:21 -0300

    deleta migrations inuteis

--- db/migrate/20191109164911_remove_devise_from_models.rb
+++
@@ -1,8 +0,0 @@
-class RemoveDeviseFromModels < ActiveRecord::Migration[5.2]
-  def change
-      drop_table :alunos
-      drop_table :docentes
-      drop_table :admins
-      drop_table :representante_externos
-  end
-end
