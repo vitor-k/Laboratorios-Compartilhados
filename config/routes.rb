b9b555e1b9c41be774a79dd303a7a58de417ad8a
Vinicius <massaki1999@gmail.com>
2019-11-22 21:33:23 -0300

    path_view_pedido

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
   resources :admins
   resources :docentes
   resources :alunos
+
   resources :pedido_responsabilidades
   get '/account/:id/pedido_responsabilidade', to: 'pedido_responsabilidades#index_docente', as: 'index_docente'
   put '/pedido_responsabilidades/aceitar/(:id)', to: 'pedido_responsabilidades#aceitar', as: 'aceitar'
+
   resources :pedidos
-  get '/pedido/:tipo/:id/:idEquipamento', to: "pedidos#new", as: 'new_pedido_alternativo'
-  post '/pedido/:tipo/:id/:idEquipamento', to: "pedidos#blabla", as: 'create_pedido_alternativo'
+  get '/pedido/:tipo/:idLab/:idItem', to: "pedidos#new", as: 'new_pedido_alternativo'
   get '/laboratorios/:idLab/pedidos', to: "pedidos#show_lab", as: 'show_laboratorio_pedidos'
   put '/laboratorios/:idLab/pedidos/aceitar/(:idPedido)', to: "pedidos#aceitar_pedido", as: 'aceitar_pedido'
+
   resources :postagems
   resources :postagems, :path => :postagens, as: :postagens
 
